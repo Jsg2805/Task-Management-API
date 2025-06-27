9c13f413bad131747a2498e33c43fe4d36777e1d
Jsg2805 <johannshonigeorge@gmail.com>
2025-06-27 17:57:18 +0530

    Create exceptions.py

---
+++ utils/exceptions.py
@@ -0,0 +1,10 @@
+class UserAlreadyExistsError(Exception):
+    pass
+
+class TaskAlreadyExistsError(Exception):
+    pass
+
+class EntityNotFoundError(Exception):
+    pass
+class TaskNotFoundError(Exception):
+    pass
